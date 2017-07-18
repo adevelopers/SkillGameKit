15342722f47ce24427e4e6a5cdb1bc03042c5bd7
adeveloper2015 <hudiakov@gmail.com>
2017-07-18 15:17:13 +0300

    Remove useless code

--- SkillGameKit/GameScene.swift
+++ SkillGameKit/GameScene.swift
@@ -25,22 +25,6 @@ class GameScene: SKScene {
         addCamera()
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-    
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-    
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-      
-    }
-    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first as UITouch! else { return }
         let acceleration: CGFloat = 1
@@ -56,33 +40,6 @@ class GameScene: SKScene {
         }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
-    }
 }
 
 extension GameScene {
